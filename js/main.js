a24b925fbfa2f71cf1a200181f47b65a90d2ffb5
Julieta Fernandez <juli47g1@stud.kea.dk>
2021-05-29 21:44:58 +0200

    menu added

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
