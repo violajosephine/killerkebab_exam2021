1bd24d493a7ce8179161dce5fc8411e863742a4d
julietafc <juli47g1@stud.kea.dk>
2021-05-30 15:05:40 +0200

    datepicker added

--- js/datepicker.js
+++ js/datepicker.js
@@ -2,9 +2,19 @@ document
   .querySelector("#takeawayOptions")
   .addEventListener("click", optionsModal);
 
+document.querySelector("#dateTimeOptions").addEventListener("click", dateModal);
+
 function optionsModal() {
   console.log("function optionsModal()");
+  document.querySelector("#optionsDate").classList.add("hidden");
   document.querySelector("#optionsTakeaway").classList.remove("hidden");
+  document.querySelector(".exit").addEventListener("click", exitWindow);
+}
+
+function dateModal() {
+  console.log("function dateModal()");
+  document.querySelector("#optionsTakeaway").classList.add("hidden");
+  document.querySelector("#optionsDate").classList.remove("hidden");
   document.querySelector(".exit").addEventListener("click", exitWindow);
 }
 
