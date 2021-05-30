c6a67ef841b9935a0594ab30dc7e2cd553be26f7
julietafc <juli47g1@stud.kea.dk>
2021-05-30 22:46:43 +0200

    dynamic info in date and time

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,16 +1,19 @@
-const exit = document.querySelectorAll(".exit");
+const urlInfo = new URLSearchParams(window.location.search);
+
+const dateTime = urlInfo.get("pickuptime");
+const timeTime = urlInfo.get("pickupdate");
+
+if (dateTime) {
+  document.querySelector("#dateTimeOptions p").textContent = dateTime;
+
+  document.querySelector(".time").textContent = timeTime;
+}
 
 document
   .querySelector("#takeawayOptions")
   .addEventListener("click", optionsModal);
 
 document.querySelector("#dateTimeOptions").addEventListener("click", dateModal);
-
-exit.forEach((n) => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-  exit.classList.remove(".active");
-}
 
 function optionsModal() {
   console.log("function optionsModal()");
@@ -23,10 +26,17 @@ function dateModal() {
   console.log("function dateModal()");
   document.querySelector("#optionsTakeaway").classList.add("hidden");
   document.querySelector("#optionsDate").classList.remove("hidden");
-  document.querySelector(".exit").addEventListener("click", exitWindow);
+  document.querySelector(".exitDate").addEventListener("click", exitWindowDate);
 }
 
 function exitWindow() {
   console.log("function exitWindow");
+  document.querySelector("#optionsTakeaway").classList.add("hidden");
+  document.querySelector("#optionsDate").classList.add("hidden");
+}
+
+function exitWindowDate() {
+  console.log("function exitWindowDate");
+  document.querySelector("#optionsDate").classList.add("hidden");
   document.querySelector("#optionsTakeaway").classList.add("hidden");
 }
