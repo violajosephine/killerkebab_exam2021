6dcc430f8f9b3f80394cfd7f917f2492f28197ab
MichGlz <migu0403@stud.kea.dk>
2021-06-01 22:04:32 +0200

    link to index modal time and takeway

--- js/datepicker.js
+++ js/datepicker.js
@@ -4,39 +4,65 @@ const dateTime = urlInfo.get("pickuptime");
 const timeTime = urlInfo.get("pickupdate");
 
 if (dateTime) {
-  document.querySelector("#dateTimeOptions p").textContent = dateTime;
+  document.querySelectorAll(".dateP").forEach((e) => {
+    e.textContent = dateTime;
+  });
 
-  document.querySelector(".time").textContent = timeTime;
+  document.querySelectorAll(".time").forEach((e) => {
+    e.textContent = timeTime;
+  });
 }
 
-document
-  .querySelector("#takeawayOptions")
-  .addEventListener("click", optionsModal);
+document.querySelectorAll(".takeawayOptions").forEach((iconBtn) => {
+  iconBtn.addEventListener("click", optionsModal);
+});
 
-document.querySelector("#dateTimeOptions").addEventListener("click", dateModal);
+document.querySelectorAll(".dateTimeOptions").forEach((e) => {
+  e.addEventListener("click", dateModal);
+});
 
 function optionsModal() {
   console.log("function optionsModal()");
-  document.querySelector("#optionsDate").classList.add("hidden");
-  document.querySelector("#optionsTakeaway").classList.remove("hidden");
-  document.querySelector(".exit").addEventListener("click", exitWindow);
+  document.querySelectorAll(".optionsDate").forEach((e) => {
+    e.classList.add("hidden");
+  });
+  document.querySelectorAll(".optionsTakeaway").forEach((e) => {
+    e.classList.remove("hidden");
+  });
+  document.querySelectorAll(".exit").forEach((e) => {
+    e.addEventListener("click", exitWindow);
+  });
 }
 
 function dateModal() {
   console.log("function dateModal()");
-  document.querySelector("#optionsTakeaway").classList.add("hidden");
-  document.querySelector("#optionsDate").classList.remove("hidden");
-  document.querySelector(".exitDate").addEventListener("click", exitWindowDate);
+  document.querySelectorAll(".optionsTakeaway").forEach((e) => {
+    e.classList.add("hidden");
+  });
+  document.querySelectorAll(".optionsDate").forEach((e) => {
+    e.classList.remove("hidden");
+  });
+  document.querySelectorAll(".exitDate").forEach((e) => {
+    e.addEventListener("click", exitWindowDate);
+  });
 }
 
 function exitWindow() {
   console.log("function exitWindow");
-  document.querySelector("#optionsTakeaway").classList.add("hidden");
-  document.querySelector("#optionsDate").classList.add("hidden");
+  document.querySelectorAll(".optionsTakeaway").forEach((e) => {
+    e.classList.add("hidden");
+  });
+  document.querySelectorAll(".optionsDate").forEach((e) => {
+    e.classList.add("hidden");
+  });
 }
 
 function exitWindowDate() {
   console.log("function exitWindowDate");
-  document.querySelector("#optionsDate").classList.add("hidden");
-  document.querySelector("#optionsTakeaway").classList.add("hidden");
+  document.querySelectorAll(".optionsDate").forEach((e) => {
+    e.classList.add("hidden");
+  });
+  document.querySelectorAll(".optionsTakeaway").forEach((e) => {
+    e.classList.add("hidden");
+  });
 }
